1dcb2336261eb09c9548856f74dc20876246165a
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2021-05-24 19:19:08 -0400

    course progress

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Import the react and reactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
+import faker from "faker";
+//Create react Componenet
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="at 4:50PM"
+          commentContent="this is comment 1"
+          imgSrc={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="at 3:20PM"
+          commentContent="this is comment 2"
+          imgSrc={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="at 4:20PM"
+          commentContent="this is comment 3"
+          imgSrc={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="at 9:20PM"
+          commentContent="this is comment 4"
+          imgSrc={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+//show componenet on screen
+ReactDOM.render(<App />, document.querySelector("#root"));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+/> */
