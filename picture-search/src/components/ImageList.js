11bafe8f99630205abd1d43664a5030670d60831
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2021-06-17 08:44:06 -0400

    update

--- picture-search/src/components/ImageList.js
+++ picture-search/src/components/ImageList.js
@@ -1,8 +1,11 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = (props) => {
-  console.log(props);
-  return <div>Image List</div>;
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img alt={description} key={id} src={urls.regular} />;
+  });
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
