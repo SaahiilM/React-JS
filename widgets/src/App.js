0a3ede259bb2b0db20b43349b7e2a2d50bd85857
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-03-01 17:12:32 -0500

    Adding Dropdown Component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -18,11 +19,32 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The color red",
+    value: "red",
+  },
+  {
+    label: "The color blue",
+    value: "blue",
+  },
+  {
+    label: "The color green",
+    value: "green",
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
