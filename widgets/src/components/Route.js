1a4ef68149f35335f1326a4815920d50a1e52a87
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-03-14 11:20:44 -0400

    implement eventListner for Link component

--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 const Route = ({ path, children }) => {
+  //defined as separate callback function to cleanup later
+  const onLocationChange = () => {
+    console.log("Location Changed");
+  };
+
+  useEffect(() => {
+    window.addEventListener("popstate", onLocationChange);
+
+    // to clean up eventListner
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
   return window.location.pathname === path ? children : null;
 };
 
