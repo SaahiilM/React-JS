e645671af0205857ba12abaaf152ce76b01403cd
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-03-14 11:31:33 -0400

    Open link in new tab when CMD+Click or CTRL+Click

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -2,6 +2,11 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    //Handle cmd+click or ctrl+click to open link in new tab.
+    //metaKey for mac, ctrlKey for windows
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     // prevent full page reload when pathname is changed using navBar
     window.history.pushState({}, "", href);
