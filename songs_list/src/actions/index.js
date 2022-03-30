98f64846776237b1a283769e34bf83db7678a498
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-03-30 02:53:42 -0400

    coding out the action creator

--- songs_list/src/actions/index.js
+++ songs_list/src/actions/index.js
@@ -1,1 +1,11 @@
 //named as index.js to make webpack import this file automatically from the directory
+
+//Action creator
+//export to do multiple functions export from the same file (named export)
+export const selectSong = (song) => {
+  //return an action
+  return {
+    type: "SONG_SELECTED",
+    payload: song,
+  };
+};
