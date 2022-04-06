94eaccf7c632e67b2e48f41a4f38202b0006a879
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-04-06 14:45:56 -0400

    nesting action creators inside another action creator to make minimum network request, using lodash uniq and map functions

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -10,6 +10,17 @@ import jsonPlaceholder from "../api/jsonPlaceholder";
 //     });
 //   };
 // };
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  //manually dispatch the inner action creator to dispatch the returned dispatch function
+  await dispatch(fetchPosts());
+  //wait for the api request of inner action creator to execute
+
+  //find unique userId
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+
+  //iterate over unique userId and call fetchUser action creator
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -20,19 +31,27 @@ export const fetchPosts = () => async (dispatch) => {
   });
 };
 
-export const fetchUser = (id) => (dispatch) => {
-  //passing the id and dispatch function as arguement to memoized function
-  _fetchUser(id, dispatch);
-};
-
-//function to make request and dispatch action
-//made separately so it gets memoized only once
-//fetches each user one time
-//doest not allow to refetch the user
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({
     type: "FETCH_USER",
     payload: response.data,
   });
-});
+};
+
+// export const fetchUser = (id) => (dispatch) => {
+//   //passing the id and dispatch function as arguement to memoized function
+//   _fetchUser(id, dispatch);
+// };
+
+// //function to make request and dispatch action
+// //made separately so it gets memoized only once
+// //fetches each user one time
+// //doest not allow to refetch the user
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data,
+//   });
+// });
