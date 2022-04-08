539bacba858dda6c22c502b307ea059f8acdb295
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-04-08 06:36:24 -0400

    Setting up the routes to different components

--- streamingApp/client/src/components/App.js
+++ streamingApp/client/src/components/App.js
@@ -1,31 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const Page1 = () => {
-  return (
-    <div>
-      Page One
-      {/* used in place of <a></a> too avoid dumping of all state and redux data as it makes a request to the server and updated index.html is fetched loaded*/}
-      <Link to="/page2">Go to Page 2</Link>
-      {/* still shows up as <a></a> when inspected in browser */}
-    </div>
-  );
-};
-const Page2 = () => {
-  return (
-    <div>
-      Page 2<Link to="/">Go to Page 1</Link>
-    </div>
-  );
-};
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={Page1} />
-          <Route path="/page2" component={Page2} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
