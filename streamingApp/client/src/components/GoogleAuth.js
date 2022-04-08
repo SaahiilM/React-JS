9885e27eda760658f94071d951c72efbc0e50d55
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-04-08 07:57:25 -0400

    checking status if user logged in or not

--- streamingApp/client/src/components/GoogleAuth.js
+++ streamingApp/client/src/components/GoogleAuth.js
@@ -1,20 +1,38 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     //   to get variable gapi available on window scope
-    // callback function execute after getting the access of google library
+    // callback function execute after getting the access of gapi library
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "410779206769-2g6md620iolj2kj871nfckh4371pqcv8.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "410779206769-2g6md620iolj2kj871nfckh4371pqcv8.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          //executed after entire gapi library is ready
+          //check user is signed in or not
+          this.auth = window.gapi.auth2.getAuthInstance();
+          //update component level state to display login status
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
+  }
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Not sure about status</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>Signed In</div>;
+    } else {
+      return <div>Not signed in</div>;
+    }
   }
 
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
