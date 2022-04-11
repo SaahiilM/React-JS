c1c6847de72b4b956632e542c76764abf97a9327
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-04-11 06:34:06 -0400

    utilizing redux form. getting input values and handling onSubmit function

--- streamingApp/client/src/components/streams/StreamCreate.js
+++ streamingApp/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form"; //Field is component that shows up on screen, reduxForm is a function similar to connect()
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  // renderInput(formProps) {
+  renderInput({ input, label }) {
+    // console.log(formProps);
+    return (
+      // <input
+      //   onChange={formProps.input.onChange}
+      //   value={formProps.input.value}
+      // ></input>
+      // <input {...formProps.input} />
+      <div className="field">
+        <label>
+          {label}
+          <input {...input} />
+        </label>
+      </div>
+    );
+  }
+  onSubmit(formValues) {
+    // event.preventDefault() handled by handleSubmit()
+    console.log(formValues);
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      // handleSubmit() is provided by redux-form
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "streamCreate", //purpose of the form
+})(StreamCreate);
