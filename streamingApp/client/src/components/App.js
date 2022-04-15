7bbd67d01053df066746a830f72e1f6afe56c668
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-04-15 10:57:32 -0400

    using <Router> in place of <BrowserRouter> to allow passing our own history objject instead of built in one which BrowserRouter has

--- streamingApp/client/src/components/App.js
+++ streamingApp/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
@@ -7,10 +7,12 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 
+import history from "../history";
+
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         {/* to display <Link> mentioned in Header.js  as it cannot be used outside of BrowserRouter*/}
         <Header />
         <div>
@@ -20,7 +22,7 @@ const App = () => {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
