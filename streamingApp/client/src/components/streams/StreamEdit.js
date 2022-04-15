e4043f33e35ba7ef5560466381d4a8b23a6fcfdd
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-04-15 13:19:55 -0400

    making use of StreaForm and wiring up the editStream action creator

--- streamingApp/client/src/components/streams/StreamEdit.js
+++ streamingApp/client/src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 // const StreamEdit = (props) => {
 //   console.log(props);
 //   return <div>StreamEdit</div>;
@@ -11,11 +13,31 @@ class StreamEdit extends React.Component {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+
+        {/* _.pick() is used to get only the fields that are to be allowed to change from the stream object */}
+        {/* initialValues is redux form specific prop as the form is wrapped inside redux form */}
+        <StreamForm
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        ></StreamForm>
+        {/* 2nd method without lodash */}
+        {/* <StreamForm
+          initialValues={{ title: this.props.stream.title, description:this.props.stream.description }}
+          onSubmit={this.onSubmit}
+        ></StreamForm> */}
+      </div>
+    );
   }
 }
 
@@ -24,4 +46,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
