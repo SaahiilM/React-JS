cac8f36fcb5b0f272eb48c8c7e91529be1645aaf
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-04-15 05:10:46 -0400

    creating new reducers for handling all REST based requests

--- streamingApp/client/src/reducers/index.js
+++ streamingApp/client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import { reducer as formReducer } from "redux-form";
+import streamReducer from "./streamReducer";
+
 export default combineReducers({
   auth: authReducer,
   form: formReducer, //only the key 'form' is to be used
+  streams: streamReducer,
 });
