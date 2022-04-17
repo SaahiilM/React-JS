2f6989c514fb301f2207aa743243ef0136c6a1c4
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-04-16 23:41:10 -0400

    setting up functionality for deleting selected stream

--- streamingApp/client/src/components/streams/StreamDelete.js
+++ streamingApp/client/src/components/streams/StreamDelete.js
@@ -1,25 +1,54 @@
 import React from "react";
 import history from "../../history";
 import Modal from "../Modal";
+import { connect } from "react-redux";
+import { fetchStream, deleteStreams } from "../../actions";
+import { Link } from "react-router-dom";
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">DELETE</button>
-      <button className="ui button">CANCEL</button>
-    </React.Fragment>
-  );
-  return (
-    <div>
-      Stream Delete
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions() {
+    const { id } = this.props.match.params;
+    return (
+      <React.Fragment>
+        <button
+          className="ui button negative"
+          onClick={() => this.props.deleteStreams(id)}
+        >
+          DELETE
+        </button>
+        <Link className="ui button" to="/">
+          CANCEL
+        </Link>
+      </React.Fragment>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to dlete this stream";
+    }
+    return `Are you sure you want to delete stream with title:  ${this.props.stream.title}`;
+  }
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Are you sure?"
-        actions={actions}
+        content={this.renderContent()}
+        actions={this.renderActions()}
         onDismiss={() => history.push("/")}
       />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStreams })(
+  StreamDelete
+);
